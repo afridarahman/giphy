4d38013bae41f44264942a121cc2a4d05a4b2e59
afridarahman <afridarahman365@gmail.com>
2019-03-19 22:16:56 +0000

    giphy day one

--- js/script.js
+++ js/script.js
@@ -2,10 +2,16 @@
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
-
+var giphyAPI = "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC";
 $("#search-button").click(function(){
-  
-  
-  
+    $.ajax({
+        url: giphyAPI,
+        method: "GET",
+        success: function(response) {
+            var gifURL = response.data[0].images.fixed_width.url;
+            $('body').append('<img src =' + gifURL + '/>');
+        }
+    });
+
 });
 
