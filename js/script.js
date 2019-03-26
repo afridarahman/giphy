81d75194236ac8932030c8deb2c736b40d88e369
afridarahman <afridarahman365@gmail.com>
2019-03-26 22:20:52 +0000

    day 2

--- js/script.js
+++ js/script.js
@@ -2,14 +2,16 @@
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
-var giphyAPI = "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC";
+
 $("#search-button").click(function(){
+    var searchTerm = $('#search-term').val();
+    var giphyAPI = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&rating=pg&api_key=dc6zaTOxFJmzC";
     $.ajax({
         url: giphyAPI,
         method: "GET",
         success: function(response) {
             var gifURL = response.data[0].images.fixed_width.url;
-            $('body').append('<img src =' + gifURL + '/>');
+            $('.text-center').html('<img src =' + gifURL + '/>');
         }
     });
 
